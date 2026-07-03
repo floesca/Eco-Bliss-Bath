ab6dc6cd5005dba03b88429ae38e710f0a988533
Florence Escanye <f.escanye@gmail.com>
2026-07-03 22:26:56 +0200

    tests api reviews et product id

--- cypress/e2e/api.cy.js
+++ cypress/e2e/api.cy.js
@@ -1,7 +1,7 @@
 const apiOrders = `${Cypress.env("apiUrl")}/orders`
 
 context("GET /orders", () => {
-  it("gets a list of products in the user's basket without being logged in", () => {
+  it("gets a list of products in the user's basket", () => {
     cy.request({
       method: "GET",
       url: apiOrders,
@@ -19,6 +19,43 @@ context("GET /products", () => {
     cy.request("GET", apiProducts).then((response) => {
       expect(response.status).to.eq(200)
       expect(response.body).length.to.be.greaterThan(1)
+    })
+  })
+})
+
+const apiRandomProducts = `${Cypress.env("apiUrl")}/products/random`
+
+context("GET /products/random", () => {
+  it("gets a list of 3 random products", () => {
+    cy.request("GET", apiRandomProducts).then((response) => {
+      expect(response.status).to.eq(200)
+      expect(response.body).to.have.length(3)
+    })
+  })
+})
+
+const apiReviews = `${Cypress.env("apiUrl")}/reviews`
+
+context("GET /reviews", () => {
+  it("gets the comments posted on the site", () => {
+    cy.request("GET", apiReviews).then((response) => {
+      expect(response.status).to.eq(200)
+      expect(response.body).length.to.be.greaterThan(2)
+    })
+  })
+})
+
+
+context("GET /products/:id", () => {
+  it("gets the product's detail", () => {
+    cy.request("GET", apiProducts).then((response) => {
+      const id = response.body[0].id
+      const apiProductId = `${Cypress.env("apiUrl")}/products/${id}`
+
+    cy.request("GET", apiProductId).then((productResponse) => {
+      expect(productResponse.status).to.eq(200)
+      expect(productResponse.body.id).to.eq(id)
+    })
     })
   })
 })
