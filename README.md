5a856770919a27b564b5d557072efae5871f9971
Florence Escanye <f.escanye@gmail.com>
2026-07-15 15:25:20 +0200

    ajout README.md

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 <div align="center">
 
-# OpenClassrooms - Eco-Bliss-Bath
+# Eco-Bliss-Bath
+Projet 10 du parcours testeur logiciel d'OpenClassrooms
 </div>
 
 <p align="center">
@@ -11,22 +12,32 @@
   <br><br><br>
 </p>
 
-# Prérequis
+## Description du projet
+
+Ce projet a pour objectif de mettre en place des tests automatisés end-to-end (E2E) avec Cypress afin de vérifier le bon fonctionnement d’une application web.
+
+Les tests réalisés permettent de simuler des parcours utilisateurs, de contrôler les comportements de l’application et de détecter d’éventuelles régressions.
+
+## Prérequis
+
 Pour démarrer cet applicatif web vous devez avoir les outils suivants:
 - Docker
 - NodeJs
 
-# Installation et démarrage
+## Installation et démarrage
+
 Clonez le projet pour le récupérer
 ``` 
 git clone https://github.com/OpenClassrooms-Student-Center/Eco-Bliss-Bath-V2.git
 cd Eco-Bliss-Bath-V2
 ```
-Pour démarrer l'API avec ça base de données.
+Pour démarrer l'API avec sa base de données
 ```
 docker compose up -d
 ```
-# Pour démarrer le frontend de l'applicatif
+
+## Pour démarrer le frontend de l'applicatif
+
 Rendez-vous dans le dossier frontend
 ```
 cd ./frontend
@@ -38,3 +49,30 @@ ou
 npm install (si vous préférez)
 ```
 
+## Pour installer et lancer Cypress
+
+````
+npm install cypress --save-dev
+```
+Puis
+```
+npx cypress open
+````
+
+## Tests réalisés
+
+- API
+- Faille XSS
+- Tests fonctionnels sur la connexion et le panier
+- Smoke tests
+
+## Organisation du projet
+
+├── cypress 
+│ ├── e2e         # Scénarios de tests 
+│ ├── fixtures    # Données utilisées dans les tests 
+│ └── support     # Commandes et configurations Cypress 
+│ 
+├── cypress.config.js 
+├── package.json 
+└── README.md
--- cypress/e2e/tests-fonctionnels.cy.js
+++ cypress/e2e/tests-fonctionnels.cy.js
@@ -3,25 +3,53 @@ describe("connexion", () => {
     it("can connect and show basket button", () => {
         cy.login()
 
-        cy.get("[data-cy='nav-link-cart']")
-        .should("be.visible")
+        cy.get("[data-cy='nav-link-cart']").should("be.visible")
     })
 })
 
-// en étant connecté,
 describe("basket", () => {
-    // vérifier un champ de disponibilité 
-    it("shows disponibility", () => {
+
+    beforeEach(() => {
         cy.login()
-
+    })
+    
+    it("shows disponibility", () => {
         cy.get("[data-cy='nav-link-cart']").click()
-
         cy.contains("Disponibilité").should("be.visible")
     })
-})
 
-// cliquer sur un des produits, le stock doit étre supérieur à un pour être ajouté
-// cliquer sur ajouter au panier,
-// le produit a été ajouté au panier,
-// le nombre du stock a diminué du nombre de produits ajoutés au panier,
-// vérifier les limites
+    it("add-to-cart button should only be visible if product is available", () => {
+        cy.visit("/#/products/3") // produit avec un stock négatif
+        cy.get("[data-cy='detail-product-add']").should("not.be.visible")
+    })
+
+    it("should add a product to the basket then decrease the stock", () => {
+        cy.visit("/#/products/5")
+        cy.get("[data-cy='detail-product-stock']")
+        .invoke("text")
+        .then((stockText) => {
+
+            const initialStock = Number(stockText.match(/\d+/)[0])
+
+            cy.get("[data-cy='detail-product-add']").click()
+
+            cy.visit("/#/cart")
+            cy.get("[data-cy='cart-line']").should("be.visible")
+
+            cy.reload()
+
+            cy.get("[data-cy='detail-product-stock']")
+            .invoke("text")
+            .then((newStockText) => {
+
+                const newStock = Number(newStockText.match(/\d+/)[0])
+
+                expect(newStock).to.eq(initialStock - 1)
+
+            })
+
+        })
+
+  })
+
+})
